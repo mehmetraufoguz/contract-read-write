d464dedaf28d100da351d3b3d17b4747590e3e97
Mehmet Rauf Oğuz <mrauf.oguz@metatime.com>
2023-06-16 11:38:38 +0300

    wagmi added

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,74 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  Page,
-  Navbar,
-  NavLeft,
-  NavTitle,
-  NavTitleLarge,
-  NavRight,
-  Link,
-  Toolbar,
-  Block,
-  BlockTitle,
-  List,
-  ListItem,
-  Button
+    Page,
+    Navbar,
+    NavTitle,
+    NavRight,
+    Block,
+    BlockTitle,
+    List,
+    Button,
+    ListInput
 } from 'framework7-react';
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { Web3Modal, useWeb3Modal } from '@web3modal/react'
+import { configureChains, createConfig, WagmiConfig, useAccount } from 'wagmi'
+import { bsc } from 'wagmi/chains'
 
-const HomePage = () => (
-  <Page name="home">
-    {/* Top Navbar */}
-    <Navbar large sliding={false}>
-      <NavLeft>
-        <Link iconIos="f7:menu" iconMd="material:menu" panelOpen="left" />
-      </NavLeft>
-      <NavTitle sliding>Contract Read-Write</NavTitle>
-      <NavRight>
-        <Link iconIos="f7:menu" iconMd="material:menu" panelOpen="right" />
-      </NavRight>
-      <NavTitleLarge>Contract Read-Write</NavTitleLarge>
-    </Navbar>
-    {/* Toolbar */}
-    <Toolbar bottom>
-      <Link>Left Link</Link>
-      <Link>Right Link</Link>
-    </Toolbar>
-    {/* Page content */}
-    <Block>
-      <p>Here is your blank Framework7 app. Let's see what we have here.</p>
-    </Block>
-    <BlockTitle>Navigation</BlockTitle>
-    <List strong inset dividersIos>
-      <ListItem link="/about/" title="About"/>
-      <ListItem link="/form/" title="Form"/>
-    </List>
+const chains = [bsc];
+const projectId = '999f30bf53975671462b2b201cb0177d';
 
-    <BlockTitle>Modals</BlockTitle>
-    <Block className="grid grid-cols-2 grid-gap">
-      <Button fill popupOpen="#my-popup">Popup</Button>
-      <Button fill loginScreenOpen="#my-login-screen">Login Screen</Button>
-    </Block>
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
+const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors: w3mConnectors({ projectId, version: 1, chains }),
+    publicClient
+})
+const ethereumClient = new EthereumClient(wagmiConfig, chains);
 
-    <BlockTitle>Panels</BlockTitle>
-    <Block className="grid grid-cols-2 grid-gap">
-      <Button fill panelOpen="left">Left Panel</Button>
-      <Button fill panelOpen="right">Right Panel</Button>
-    </Block>
 
-    <List strong inset dividersIos>
-      <ListItem
-        title="Dynamic (Component) Route"
-        link="/dynamic-route/blog/45/post/125/?foo=bar#about"
-      />
-      <ListItem
-        title="Default Route (404)"
-        link="/load-something-that-doesnt-exist/"
-      />
-      <ListItem
-        title="Request Data & Load"
-        link="/request-and-load/user/123456/"
-      />
-    </List>
-  </Page>
-);
+const HomePage = () => {
+    const { address } = useAccount();
+    const [contractAddress, setAddress] = useState('');
+    const [contractABI, setABI] = useState('');
+
+    const { open } = useWeb3Modal();
+
+    const ContractFunctions = (type) => {
+        if(address && (contractAddress.length > 0) && (contractABI.length > 0)){
+            let _contractABI;
+            try{
+                _contractABI = JSON.parse(contractABI);
+            } catch (err) {
+                return (
+                    <p>Provided ABI is invalid.</p>
+                )
+            }
+            return (
+                <p>test</p>
+            )
+        }else{
+            return (
+                <p>You have connect wallet and set contract settings.</p>
+            )
+        }
+    }
+
+    return (
+        <Page name='home'>
+            <Navbar>
+                <NavTitle sliding>Contract Read-Write</NavTitle>
+                <NavRight>
+                    <Button onClick={() => open()} color={
+                        (address > 0 ? 'green' : 'blue')
+                    } iconF7='wallet' text={
+                        (address > 0 ? address.slice(0,18) + '...' : 'Connect Wallet')
+                    } />
+                </NavRight>
+            </Navbar>
+            <Block>
+                <BlockTitle>Read Functions</BlockTitle>
+                {ContractFunctions('read')}
+            </Block>
+            <Block>
+                <BlockTitle>Write Functions</BlockTitle>
+                {ContractFunctions('write')}
+            </Block>
+            <Block>
+                <BlockTitle>Settings</BlockTitle>
+                <List>
+                    <ListInput
+                        outline
+                        label='Address'
+                        placeholder='0x'
+                        clearButton
+                        onInput={(e) => {
+                            setAddress(e.target.value)
+                        }}
+                    />
+                    <ListInput
+                        outline
+                        label='ABI'
+                        type='textarea'
+                        placeholder='[]'
+                        clearButton
+                        onInput={(e) => {
+                            setABI(e.target.value)
+                        }}
+                    />
+                </List>
+            </Block>
+            <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+        </Page>
+    );
+};
 export default HomePage;
